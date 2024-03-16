b1f93502cdd77f50ddfffde17460e31d4120371b
clairedelachaux <62568093+clairedelachaux@users.noreply.github.com>
2024-03-17 00:14:37 +0100

    Update NewtonMethod.m

--- claire/NewtonMethod.m
+++ claire/NewtonMethod.m
@@ -1,28 +1,42 @@
-function u = NewtonMethod(u0, eps, h, alpha, beta, tol, k_max)
+function [u, err, l_delta] = NewtonMethod(u0, eps, h, alpha, beta, tol, k_max)
+
+  #Initialization of u
   u = u0;
   n = size(u0)(1);
   G = zeros(n, 1);
-  u_prime = [0; u; 0];
+  large_u = [0; u; 0];
+  l_delta=zeros(100, 1);
+  k=0;
+
+  #Calculation of G
   for j=2:(n-1)
     G(j) = eps*(u(j-1)-2*u(j)+u(j+1))/(h^2)+u(j)*((u(j+1)-u(j-1))/(2*h)-1);
   endfor
-  k=0;
-  G
-  norm(G)
+
+
   while norm(G)>tol && k<k_max
+
+    #Calculation of J
     d1 = eps*ones(n-1, 1)/(h^2)-u(2:n)/(2*h);
-    d2 = (-2*eps/(h^2)-1)*ones(n, 1)+(u_prime(3:(n+2))-u_prime(1:n))/(2*h);
+    d2 = (-2*eps/(h^2)-1)*ones(n, 1)+(large_u(3:(n+2))-large_u(1:n))/(2*h);
     d3 = eps*ones(n-1, 1)/(h^2)+u(1:(n-1))/(2*h);
-    J = diag(d1, -1)+diag(d2, 0)+diag(d3, 1);
+    d4 = [0; d3];
+    J = spdiags(d1, -1, n, n)+spdiags(d2, 0, n, n)+spdiags(d4, 1, n, n);
+
+    #Calculation of delta
     delta = J\G;
+
+    #Correction of u
     u = u-delta;
     u(1)=alpha;
     u(n)=beta;
+
+    #update the variable
     k = k+1;
     for j=2:(n-1)
       G(j) = eps*(u(j-1)-2*u(j)+u(j+1))/(h^2)+u(j)*((u(j+1)-u(j-1))/(2*h)-1);
     endfor
-    u_prime = [0; u; 0];
+    large_u = [0; u; 0];
+    l_delta(k) = norm(delta, inf);
   endwhile
-  k
-
+  err = norm(J\G, Inf);
