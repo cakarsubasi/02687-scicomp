e9d4e59b2fd81a37da60124ac73ee39f71dcc6f6
clairedelachaux <62568093+clairedelachaux@users.noreply.github.com>
2024-03-17 00:15:08 +0100

    Update ex2a.m

--- claire/ex2a.m
+++ claire/ex2a.m
@@ -1,19 +1,50 @@
+##constante
 a=0;
 b=1;
 alpha = -1;
 beta = 1.5;
 epsilon = 0.1;
-tol = 0.0001;
-kmax = 50;
-n = 30;
-h = (b-a)/(n-1);
+tol = 0.00001;
+kmax = 10000;
+
+
+##calculation of the solution for u=1
+n=100;
+h = 1/(n-1);
 omega0 = (a-b-alpha+beta)/2;
 x = (a:h:b)';
-x
 xbar =(a+b-alpha-beta)/2*ones(n, 1);
-xbar
 u0 = x - xbar + omega0*tanh(omega0*(x-xbar)/(2*epsilon));
-u0
-u = NewtonMethod(u0, epsilon, h, alpha, beta, tol, kmax);
-u
-plot(x, u)
+[u, error, delta] = NewtonMethod(u0, epsilon, h, alpha, beta, tol, kmax);
+plot(x, u) %plot the solution
+n_delta = length(delta);
+semilogy(1:n_delta, delta); %plot the convergence
+
+
+##calculation of the errors with regard to h
+nb_iter= 20;
+N=logspace(1, 3.5, nb_iter);
+H = zeros(1, nb_iter);
+e = ones(1, nb_iter);
+for k = 1:nb_iter
+  n = round(N(k));
+  h = (b-a)/(n-1);
+  H(k) = h;
+  omega0 = (a-b-alpha+beta)/2;
+  x = (a:h:b)';
+  xbar =(a+b-alpha-beta)/2*ones(n, 1);
+  u0 = x - xbar + omega0*tanh(omega0*(x-xbar)/(2*epsilon));
+  [u, err, delta] = NewtonMethod(u0, epsilon, h, alpha, beta, tol, kmax);
+  e(k) = err;
+endfor
+
+p = polyfit(H, e, 1);
+logH = log(H);
+[p, error] = polyfit(logH(4:nb_iter), log(e(4:nb_iter)), 1);
+P = polyval(p, logH);
+loglog(H, e)
+hold on
+loglog(H, exp(P))
+xlabel('log(h)')
+ylabel('log(E)')
+legend('Global error estimate', 'Linear regression','Location','southeast')
