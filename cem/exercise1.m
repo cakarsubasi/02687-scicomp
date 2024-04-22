5236a3abd925c0a5df1e909bc6c335370b1d532d
Cem Akarsubaşı <cemakarsubasi@gmail.com>
2024-04-22 11:39:24 +0200

    Experimental picard_lindeloef optimizer for exercise 1

--- cem/exercise1.m
+++ cem/exercise1.m
@@ -1,7 +1,18 @@
 delta = 0.0002;
-rhs = @(t, y) y^2 - y^3;
+rhs = @(t, y) y.^2 - y.^3;
 %rhs_u = @(t, y) 2*y - 3*y^2;
-%opts = odeset('RelTol', 1.e-6);
-%ode23s(rhs, [0 2/delta], delta, opts);
-[T, Y] = ode_solver(rhs, [0 2/delta], delta, 1.e-6, 1.e-3);
+reltol = 1.e-6;
+abstol = 1.e-4;
+
+[T, Y] = ode_solver(rhs, [0 2/delta], delta, reltol, abstol);
 plot(T, Y, "o-");
+hold on;
+%%
+opts = odeset('RelTol', reltol, "AbsTol", abstol);
+[Tref, Yref] = ode23s(rhs, [0 2/delta], delta, opts);
+plot(Tref, Yref, "o-");
+
+[Tref2, Yref2] = ode45(rhs, [0 2/delta], delta, opts);
+%plot(Tref2, Yref2, "o-");
+
+legend(["ours", "ode23s", "ode45"])
--- cem/ode_solver.m
+++ cem/ode_solver.m
@@ -5,7 +5,6 @@ function [TOUT, YOUT] = ode_solver(ODEFUN, TSPAN, Y0, reps, aeps)
 %   * TSPAN two element vector specifying beginning and end points
 %   * Y0 column vector of initial conditions
 
-% apply Picard-Lindeloff to determine step size
 tstart = TSPAN(1);
 tend = TSPAN(2);
 
@@ -15,9 +14,9 @@ y_n = Y0;
 
 % apply the RK method
 
-A1 = [0  0 0;
-     0.5 0 0;
-     0.5 0.5 0.0];
+A1 = [0    0   0  ;
+      0.5  0   0  ;
+      0.5  0.5 0.0 ];
 B1 = [1/6 2/3 1/6];
 
 A2 = [0.0 0.0 0.0;
@@ -35,20 +34,20 @@ while t_n < tend
     
     err = y_new2 - y_new1;
     
-    if norm(err) > reps*norm(y_new2) + aeps
+    if norm(err) > abs(norm(y_new2)*reps) + abs(aeps)
         step = step/2;
     else
         t_n = t_n + step;
         step = tend - t_n;
         y_n = y_new1;
+        T_star = picard_lindeloef(t_n, tend, y_n, ODEFUN);
+        step = T_star;
         YOUT = cat(2, YOUT, y_new1);
         TOUT = cat(2, TOUT, t_n);
     end
+end %while
 
-end
-
-
-end
+end %function ode_solver
 
 function y_new = rk23(A, B, y_n, t_n, odefun, step)
 a21 = A(2, 1);
@@ -68,4 +67,25 @@ ksi2 = y_n + a21*step*odefun(t_n, ksi1);
 ksi3 = y_n + a31*step*odefun(t_n, ksi1) + a32*step*odefun(t_n+c2*step, ksi2);
 
 y_new = y_n + step*(b1*odefun(t_n, ksi1) + b2*odefun(t_n+c2*step, ksi2) + b3*odefun(t_n+c3*step, ksi3));
+end
+
+function T_star = picard_lindeloef(t0, t1, y0, fun)
+T_star = 0;
+a = 0.1;
+while true
+
+% optimize S
+S = max(abs(fun(0, linspace(y0-a, y0+a, 100))));
+
+T_star_new = a/S;
+if (T_star_new < T_star) || (t0 + T_star_new >= t1)
+    break
+else
+    T_star = T_star_new;
+    a = a + 0.1;
+end
+end
+
+T_star = min(t1, t0 + T_star);
+
 end
