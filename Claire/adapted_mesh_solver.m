78865fb52d3a894d955f0cbd9c7c52e39815921f
clairedelachaux <62568093+clairedelachaux@users.noreply.github.com>
2024-05-08 12:45:29 +0200

    Update adapted_mesh_solver.m

--- Claire/adapted_mesh_solver.m
+++ Claire/adapted_mesh_solver.m
@@ -15,19 +15,19 @@ k = min(h)^2 / (2*eps);
 
 e = ones(m-1, 1);
 z = zeros(m-1, 1);
-H = sparse(1:m, 1:m, h.^(-1));
+H = sparse(1:m-1, 1:m-1, (h(1:end-1)+h(2:end))/2);
 
 A1 = spdiags([z -e e], [-1 0 1], m-1, m-1);
 A2 = spdiags([-e e z], [-1 0 1], m-1, m-1);
 U = reshape(U, [m-1 1]);
 
 %estimation of the first and second derivative of U wtr x
-dxU = (H(2:end, 2:end)*A1*U.*U+H(1:end-1, 1:end-1)*A2*U.*U)/2;
-dx2U =  H(2:end, 2:end)*H(2:end, 2:end)*A1*U-H(1:end-1, 1:end-1)*H(1:end-1, 1:end-1)*A2*U;
+dxU = (H^(-1)*A1*U.*U+H^(-1)*A2*U.*U)/2;
+dx2U =  H^(-2)*A1*U-H^(-2)*A2*U;
 
 G = zeros(m-1, 1);
-G(1) = -k*H(1)*gL^2/2+k*eps*H(1)*H(1)*gL;
-G(end) = k*H(end)*gR^2/2+k*eps*H(end)*H(end)*gR;
+G(1) = -k*gL^2/H(1)+k*eps*gL/H(1)^2;
+G(end) = k*gR^2/H(end)+k*eps*gR/H(end)^2;
 
 U_new = U + k*eps*dx2U-k*dxU+G;
 U_new = reshape(U_new, [m-1 1]);
