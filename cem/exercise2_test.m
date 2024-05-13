e64d8c835979717c99f0aa38f08710bb5af8748e
Cem Akarsubaşı <cemakarsubasi@gmail.com>
2024-05-13 17:57:10 +0200

    Add exercise 2 convergence test

--- cem/exercise2_test.m
+++ cem/exercise2_test.m
@@ -6,7 +6,17 @@ eps = 0.1;
 u_actual = @(t, x) sum(exp(-eps*alpha.^2.*t).*(a.*cos(alpha.*x)+b.*sin(alpha.*x)), 1);
 
 %%
-m = 64;
+
+start = 3;
+total = 8;
+
+mvals = 2.^(linspace(start, start+total-1, total));
+errors = zeros(total, 1);
+
+for iter = 1:total
+figure;
+m = mvals(iter);
+
 points = linspace(-1, 1, m+2);
 U_initial = u_actual(0.0, points(2:end-1));
 G = [u_actual(0, points(1)), u_actual(0, points(end))];
@@ -15,19 +25,18 @@ G = [u_actual(0, points(1)), u_actual(0, points(end))];
 step_fac = 1.0;
 
 err = 0;
-err_m = zeros([1 maxstep]);
 
-maxstep = 600;
-plotinterval = 10;
+maxstep = 200000;
+plotinterval = 1000;
 to_plot = true;
-tmax = 1;
-wait = 0.1;
+wait = 0.0;
 
 time = 0;
 U_new = U_initial;
 for i = 1:maxstep
     kmax = tmax - time;
     if kmax == 0
+        plotmain(points, U_new, U_real, G, err, i, time, wait);
         break
     end
     [U_new, k] = ftcs_solver(U_new, G, m, eps, step_fac, kmax);
@@ -35,16 +44,26 @@ for i = 1:maxstep
     U_real = u_actual(time, points);
     G = [u_actual(time, points(1)), u_actual(time, points(end))];
 
-    err = step_err(U_real, U_new);
-    err_m(i) = err;
+    err = step_err([G(1); U_new; G(2)], U_real, m+2);
 
     if mod(i, plotinterval) == 0 && to_plot
         plotmain(points, U_new, U_real, G, err, i, time, wait);
     end
 end
 
-plotmain(points, U_new, U_real, G, err, i, time, wait);
+errors(iter) = err;
+end
+%%
+figure;
+loglog(1./mvals, errors, "o-");
+title("Convergence test: heat-diffusion")
+xlabel("h");
+ylabel("error (Mean Absolute Error)");
+grid on;
+fig = gcf;
+exportgraphics(fig, "ex2_convergence.pdf");
 
+%%
 function plotmain(points, U_new, U_real, G, err, iter, time, wait)
         plot(points, [G(1); U_new; G(2)]);
         hold on;
@@ -57,6 +76,8 @@ function plotmain(points, U_new, U_real, G, err, iter, time, wait)
         pause(wait);
 end
 
-function err = step_err(est, act)
-    err = sum(abs(act - est), "all") / length(est);
+function err = step_err(est, act, n)
+    act = reshape(act, [n, 1]);
+    est = reshape(est, [n, 1]);
+    err = sum(abs(act - est), "all") / n;
 end
--- cem/ftcs_solver.m
+++ cem/ftcs_solver.m
@@ -1,14 +1,21 @@
-function [U_new, k] = ftcs_solver(U, G, m, eps, step_fac)
+function [U_new, k] = ftcs_solver(U, G, m, eps, step_fac, kmax)
 %FTCS_SOLVER our ftcs solver
 %   * U: u values for previous iteration [1 x m]
 %   * G: boundary values [2 x 1]
 %   * m: number of values
 %   * eps:
-%   * step_fac:
+%   * step_fac: limit or increase allowed steps, setting it above 1.0 will
+%   cause instability
+%   * kmax: maximum allowed step, use it to stop an iteration at a specific
+%   point
 
 % pick h and k values
 h = 2/(m+1);
 k = h^2 / (2*eps)*step_fac;
+if k > kmax
+    k = kmax;
+end
+
 cons = eps*k/h^2;
 
 U = reshape(U, [m, 1]);
@@ -19,7 +26,5 @@ e = ones(m, 1);
 A = spdiags([1*e -2*e 1*e], [-1 0 1], m, m);
 
 U_new = U + A*U*cons + G*cons;
-disp(size(U_new));
-
 end
 
